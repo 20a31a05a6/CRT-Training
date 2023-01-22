6613e6f548eb49d3ea7ee0ee15729b149bcfc8b9
20a31a05a6 <109963842+20a31a05a6@users.noreply.github.com>
2023-01-22 20:51:54 +0530

    first commit

---
+++ Day3.py
@@ -0,0 +1,12 @@
+row=3
+col=3
+arr=[]
+for i in range(row):
+    element=[]
+    for j in range(col):
+        element.append((int(input("enter element"))))
+    arr.append(ele)
+print(arr)
+
+
+        
